f6993493c0e50d9d07d17694fd5aba3f757c30b7
MarcosKip06 <markinhos.a17@gmail.com>
2024-08-05 14:50:27 -0300

    primeiro commit

---
+++ src/main/java/com/crud/model/Reservation.java
@@ -0,0 +1,29 @@
+package com.crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String rooName;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String reservedBy;
+
+}
